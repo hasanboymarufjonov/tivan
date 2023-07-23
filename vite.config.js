bf8afcc80486f50ed9ac41686ba434ba201f4d65
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-07-24 00:51:06 +0500

    vite config fixing 5

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig({
         // target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
